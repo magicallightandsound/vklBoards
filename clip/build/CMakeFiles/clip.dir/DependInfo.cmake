06d6eea8003d1bdcda6f60ec736003fffb128c14
Victor Liu <victor.liu@gmail.com>
2020-03-23 12:21:23 -0700

    Re-added clip.

---
+++ clip/build/CMakeFiles/clip.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/vliu/code/vklBoards/clip/clip.cpp" "/Users/vliu/code/vklBoards/clip/build/CMakeFiles/clip.dir/clip.cpp.o"
+  "/Users/vliu/code/vklBoards/clip/clip_osx.mm" "/Users/vliu/code/vklBoards/clip/build/CMakeFiles/clip.dir/clip_osx.mm.o"
+  "/Users/vliu/code/vklBoards/clip/image.cpp" "/Users/vliu/code/vklBoards/clip/build/CMakeFiles/clip.dir/image.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
